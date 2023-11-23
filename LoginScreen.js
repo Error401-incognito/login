e96f66fbbe981ddaf88f964a50573c642c5d0242
Error401-incognito <98617109+Error401-incognito@users.noreply.github.com>
2023-11-23 20:31:54 +0530

    Create LoginScreen.js

---
+++ LoginScreen.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add your authentication logic here
+    // For simplicity, just navigate to the HomeScreen for any input
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    marginVertical: 10,
+    padding: 10,
+    borderWidth: 1,
+  },
+});
+
+export default LoginScreen;
